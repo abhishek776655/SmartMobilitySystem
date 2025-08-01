9fbcc26afdacb691ec0d7ac6c6df534cd3cf82a7
abhishek776655 <abhishek.pandey9868@gmail.com>
2025-08-01 21:32:14 +0530

    register route

--- auth-service/src/main/java/com/mobility/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/mobility/authservice/controller/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.mobility.authservice.dto.ApiResponse;
 
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -36,19 +35,37 @@ public class AuthController {
         this.registerService = registerService;
     }
 
-     @PostMapping("/register")
-     public ResponseEntity<ApiResponse<UserResponseDTO>> register(@Valid @RequestBody RegisterUserRequest request){
-        System.out.println(request.getRole());
-        System.out.println(RoleType.RIDER.toString());
-        if (request.getRole()== null || !(request.getRole().equals(RoleType.RIDER.toString()) || request.getRole().equals(RoleType.DRIVER.toString())))
-        {
+sntroller
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final IAuthService authService;
+    private final IRegisterService registerService;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserClient userClient;
+
+    @Autowired
+    public AuthController(IAuthService authService, IRegisterService registerService) {
+        this.authService = authService;
+        this.registerService = registerService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<UserResponseDTO>> register(@Valid @RequestBody RegisterUserRequest request) {
+        if (request.getRole() == null || !(request.getRole().equals(RoleType.RIDER.toString())
+                || request.getRole().equals(RoleType.DRIVER.toString()))) {
             System.out.println(request.getRole().equals(RoleType.RIDER.toString()));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("Role must be RIDER or DRIVER.",null));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.error("Role must be RIDER or DRIVER.", null));
         }
         UserResponseDTO user = registerService.register(request);
-     return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.<UserResponseDTO>success("User Registered", user));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.<UserResponseDTO>success("User Registered", user));
 
-     }
+    }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody @Valid LoginRequest request) {
--- auth-service/src/main/java/com/mobility/authservice/service/Impl/AuthService.java
+++ auth-service/src/main/java/com/mobility/authservice/service/Impl/AuthService.java
@@ -7,7 +7,6 @@ import com.mobility.authservice.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +15,9 @@ public class AuthService implements IAuthService {
     public final UserClient userClient;
     public final AuthenticationManager authenticationManager;
     public final JwtUtils jwtUtils;
+
     @Autowired
-    public AuthService(UserClient userClient, AuthenticationManager authenticationManager, JwtUtils jwtUtils){
+    public AuthService(UserClient userClient, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.userClient = userClient;
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
@@ -25,10 +25,11 @@ public class AuthService implements IAuthService {
 
     @Override
     public AuthResponse login(LoginRequest request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         ApiResponse<UserResponseDTO> userResponse = userClient.getUserByEmail(request.getEmail());
         UserResponseDTO user = userResponse.getData();
         String token = jwtUtils.generateToken(user);
-        return new AuthResponse(token,user.getId(),user.getRole().toString(),user.getEmail());
+        return new AuthResponse(token, user.getId(), user.getRole().toString(), user.getEmail());
     }
 }
